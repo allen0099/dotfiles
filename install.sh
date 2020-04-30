0a0245a51711d541cf5792a049dac84f84c8bcb5
秉虎 <s96016641@gmail.com>
2020-04-30 18:38:37 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,110 +2,10 @@
 
 # Defines
 SCRIPT=$(readlink -f "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
+SCRIPT_PATH=$(dirname "$SCRIPT")
 
 # Checking distribution
 source ./check_distribution.sh
-
-# Needed software list
-target_software=(git zsh vim tmux)
-configs=(.bashrc .gitconfig)
-vim_require=(git curl python3-pip exuberant-ctags ack-grep)
-zsh_require=(autojump)
-
-echo "[INFO] Your distribution is ${distribution:?} ${distribution_version:?}"
-
-function initial() {
-    if [ "$distribution" == "Ubuntu" ]; then
-        permission=$USER
-        current_user=$SUDO_USER
-        if [ "$distribution_version" == "20.04" ]; then
-            home_directory="/home/$SUDO_USER"
-        else
-            home_directory=$HOME
-        fi
-    elif [ "$distribution" == "CentOS Linux" ]; then
-        permission=$USER
-        current_user=$SUDO_USER
-        home_directory="/home/$SUDO_USER"
-    else
-        echo "[ERROR] Your distribution haven't been support yet. Exit."
-        exit 1
-    fi
-    echo "[DEBUG] USER=$USER"
-    echo "[DEBUG] SUDO_USER=$SUDO_USER"
-    echo "[DEBUG] HOME=$HOME"
-    echo "[DEBUG] permission=$permission"
-    echo "[DEBUG] current_user=$current_user"
-    echo "[DEBUG] home_directory=$home_directory"
-    # permission check
-    if [ "$permission" != "root" ]; then
-        echo "[ERROR] You need to be sudo...! Exit."
-        exit 1
-    fi
-    echo "[INFO] update git submodule..."
-    git submodule update --init --recursive
-    if [ "$HOME" != "$home_directory" ]; then
-        HOME=$home_directory
-        echo "[INFO] home fixed!"
-        echo "[DEBUG] HOME=$HOME"
-        echo "[DEBUG] home_directory=$home_directory"
-    fi
-}
-
-function ln_conf() {
-    dst="$home_directory/$1"
-    if [ -f "$dst" ] || [ -d "$dst" ]; then
-        echo "[WARNING] File conflict: $dst already existed!"
-        read -rp "[WARNING] move conflict to $dst.bak (y/N)?" choice
-        case "$choice" in
-            y|Y )
-                echo "[INFO] Moving file..."
-                mv "$dst" "$dst.bak"
-                src="$SCRIPTPATH/$1"
-                echo "[INFO] Link $src to $dst"
-                ln -s "$src" "$dst"
-                chown -h "$current_user":"$current_user" "$dst"
-                ;;
-            n|N )
-                echo "[INFO] Do nothing! Not link the file!"
-                ;;
-            * )
-                echo "[ERROR] Invalid options, disrupted"
-                ;;
-        esac
-    else
-        src="$SCRIPTPATH/$1"
-        echo "[INFO] Link $src to $dst"
-        ln -s "$src" "$dst"
-        chown -h "$current_user":"$current_user" "$dst"
-    fi
-}
-
-function check_software() {
-    read -rp "[INFO] Install $1... (y/N)?" choice
-    case "$choice" in
-        y|Y )
-            echo "[INFO] Checking $1..."
-            if [ -x "$(command -v "$1")" ]; then
-                echo "[INFO] Done!"
-            else
-                echo "[INFO] $1 is not installed. Installing..."
-                if [ "$distribution" == "Ubuntu" ]; then
-                    apt install -y "$1"
-                elif [ "$distribution" == "CentOS Linux" ]; then
-                    yum -y install "$1"
-                fi
-            fi
-            ;;
-        n|N )
-            echo "[INFO] Don't install $1"
-            ;;
-        * )
-            echo "[ERROR] Invalid options, disrupted"
-            ;;
-    esac
-}
 
 echo "[INFO] "
 echo "[INFO]   +------------------------------------------------+"
@@ -116,88 +16,188 @@ echo "[INFO]   +------------------------------------------------+ |"
 echo "[INFO]    \\______________________________________________\\|"
 echo "[INFO] "
 
-initial
+echo "[INFO] Your distribution is ${distribution:?} ${distribution_version:?}"
 
-# Check and install software
-for software in "${target_software[@]}"; do
-    check_software "$software"
-done
-
-for software in "${configs[@]}"; do
-    ln_conf "$software"
-done
-
-# Check vim and install require
-if [ -x "$(command -v vim)" ]; then
-    echo "[INFO] Install vim require..."
-    for software in "${vim_require[@]}"; do
-        check_software "$software"
-    done
-    echo "[INFO] fisa-vim requirements installing..."
-    pip3 install pynvim flake8 pylint isort
-    ln_conf .vimrc
-    vim +qall
-
-    echo "[INFO] Changing permission to $current_user"
-    chown -R "$current_user":"$current_user" "$HOME/.vim"
-    chown -R "$current_user":"$current_user" "$HOME/.fzf"
-    chown -R "$current_user":"$current_user" "$HOME/.fzf.bash"
-    chown -R "$current_user":"$current_user" "$HOME/.fzf.zsh"
-else
-    echo "[ERROR] Vim is not installed. Aborting..."
-fi
-
-# Check tmux and install require
-if [ -x "$(command -v tmux)" ]; then
-    echo "[INFO] Install tmux configuration..."
-    ln_conf .tmux.conf
-    ln_conf .tmux.conf.local
-else
-    echo "[ERROR] Tmux is not installed. Aborting..."
-fi
-
-# Check zsh and install require
-if [ -x "$(command -v zsh)" ]; then
-    echo "[INFO] Install oh-my-zsh plugins require..."
-    for software in "${zsh_require[@]}"; do
-        check_software "$software"
-    done
-    echo "[INFO] Install oh-my-zsh..."
-    ./oh-my-zsh/tools/install.sh
-    echo "[INFO] Install oh-my-zsh theme..."
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$home_directory/.oh-my-zsh/custom/themes/powerlevel10k"
-    echo "[INFO] Install oh-my-zsh plugins..."
-    git clone https://github.com/zsh-users/zsh-autosuggestions "$home_directory/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting "$home_directory/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
-    echo "[INFO] Setting up the zsh config..."
-    # sed -i "22s:\$HOME:$home_directory:" .zshrc
-    ln_conf .p10k.zsh
-    ln_conf .zshrc
-
-    echo "[INFO] Changing permission to $current_user"
-    chown -R "$current_user":"$current_user" "$HOME/.oh-my-zsh"
-else
-    echo "[ERROR] Zsh is not installed. Aborting..."
-fi
-
-# Force Android emulator use system libs
-if [ "$distribution" == "Ubuntu" ]; then
-    if [ -f ~/.pam_environment ]; then
-        read -rp "[INFO] Force Android emulator use system libs (y/N)?" choice
-        case "$choice" in
-            y|Y ) echo "ANDROID_EMULATOR_USE_SYSTEM_LIBS=1" >> ~/.pam_environment;;
-            n|N ) echo "Don't add environment";;
-            * ) echo "Invalid options, disrupted";;
-        esac
+function chk_sfw() {
+  for software in "$@"; do
+    echo "[INFO] Checking $software ..."
+    if [ -x "$(command -v "$software")" ]; then
+      echo "[INFO] $software has been installed successfully!"
+    else
+      echo "[INFO] $software is not installed. Installing..."
+      if [ "$distribution" == "Ubuntu" ]; then
+        sudo apt install -y "$software"
+      fi
     fi
-fi
+  done
+}
 
-# If you have faced some QT error, can't input Chewing or other CJK
-# uncomment code below may help you
-#echo "GTK_IM_MODULE=ibus" >> ~/.pam_environment
-#echo "QT_IM_MODULE=ibus" >> ~/.pam_environment
-#echo "XMODIFIERS=@im=ibus" >> ~/.pam_environment
+function chk_ln() {
+  dst="$HOME/$1"
+  if [ -f "$dst" ] || [ -d "$dst" ]; then
+    echo "[WARNING] File conflict: $dst already existed!"
+    read -rp "[WARNING] move conflict to $dst.bak (y/N)?" choice
+    case "$choice" in
+    y | Y)
+      echo "[INFO] Moving file..."
+      mv "$dst" "$dst.bak"
+      src="$SCRIPT_PATH/$1"
+      echo "[INFO] Link $src to $dst"
+      ln -s "$src" "$dst"
+      ;;
+    n | N)
+      echo "[INFO] Do nothing! Not link the file!"
+      ;;
+    *)
+      echo "[ERROR] Invalid options, disrupted"
+      ;;
+    esac
+  else
+    src="$SCRIPT_PATH/$1"
+    echo "[INFO] Link $src to $dst"
+    ln -s "$src" "$dst"
+  fi
+}
 
-## Switch to zsh
-#echo "Change default shell to zsh"
-#chsh -s /bin/zsh $current_user
+function cfg_link() {
+  chk_ln .bashrc
+  chk_ln .gitconfig
+}
+
+function install_vim() {
+  echo "[INFO] Installing fisa-vim..."
+  chk_sfw git curl python3-pip exuberant-ctags ack-grep
+  sudo pip3 install pynvim flake8 pylint isort
+  chk_ln .vimrc
+  vim +qall
+}
+
+function install_tmux() {
+  echo "[INFO] Installing tmux..."
+  chk_sfw tmux
+  chk_ln .tmux.conf
+  chk_ln .tmux.conf.local
+}
+
+function install_zsh() {
+  echo "[INFO] Installing zsh..."
+  chk_sfw zsh
+
+  echo "[INFO] Installing ohmyzsh..."
+  chk_sfw curl
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+  echo "[INFO] Installing ohmyzsh theme (powerlevel10k)..."
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$HOME/.oh-my-zsh/custom/themes/powerlevel10k"
+
+  echo "[INFO] Installing ohmyzsh plugins..."
+  chk_sfw autojump
+  git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
+  chk_ln .p10k.zsh
+  chk_ln .zshrc
+}
+
+function other() {
+  clear
+  echo "****** Other option ******"
+  echo "(1)  Force Android emulator use system libs"
+  echo "(2)  Change default shell to zsh"
+  echo "(3)  QT input module (ibus)"
+  echo ""
+  echo "(q)  Leave"
+
+  read -r other_choose
+  case "$other_choose" in
+  1)
+    if [ -f ~/.pam_environment ]; then
+      echo "ANDROID_EMULATOR_USE_SYSTEM_LIBS=1" >>~/.pam_environment
+    fi
+    echo "[INFO] Done!"
+    read -rp "Press [Enter] to continue..."
+    other
+    ;;
+  2)
+    chsh -s "$(command -v zsh)" "$USER"
+    other
+    ;;
+  3)
+    if [ -f ~/.pam_environment ]; then
+      echo "GTK_IM_MODULE=ibus" >>~/.pam_environment
+      echo "QT_IM_MODULE=ibus" >>~/.pam_environment
+      echo "XMODIFIERS=@im=ibus" >>~/.pam_environment
+    fi
+    other
+    ;;
+  q | Q)
+    main
+    ;;
+  *)
+    echo "[ERROR] Invalid option, aborting..."
+    main
+    ;;
+  esac
+}
+
+function main() {
+  clear
+  echo "****** Main menu (which to install) ******"
+  echo "(1)  vim"
+  echo "(2)  tmux"
+  echo "(3)  zsh"
+  echo ""
+  echo "(c)  config link"
+  echo "(a)  All of above"
+  echo "(o)  Others (some settings)"
+  echo "(q)  Leave"
+
+  read -r main_choose
+  case "$main_choose" in
+  1)
+    install_vim
+    main
+    ;;
+  2)
+    install_tmux
+    main
+    ;;
+  3)
+    install_zsh
+    main
+    ;;
+  a | A)
+    install_vim
+    install_tmux
+    install_zsh
+    cfg_link
+    main
+    ;;
+  c | C)
+    cfg_link
+    main
+    ;;
+  o | O)
+    other
+    ;;
+  q | Q)
+    return 0
+    ;;
+  *)
+    echo "[ERROR] Invalid option, aborting..."
+    return 1
+    ;;
+  esac
+}
+
+function init() {
+  # download submodules
+  echo "[INFO] submodule initializing..."
+  chk_sfw git
+  git submodule update --init --recursive
+  echo "[INFO] submodule initialized!"
+
+  read -rp "Press [Enter] to continue..."
+  main
+}
+
+init
